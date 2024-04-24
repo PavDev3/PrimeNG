4758b492dd39d8a48c78d9182c806ac404e2d5c4
Pavdev3 <pnunfe@gmail.com>
2024-04-24 14:25:04 +0200

    Add PrimeNG components and update basics-page component

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
+import { PanelModule } from 'primeng/panel';
+import { ProductsComponent } from '../../products.component';
 
 @Component({
   standalone: true,
   selector: 'app-basics-page',
+  imports: [
+    ProductsComponent,
+    ButtonModule,
+    CardModule,
+    FieldsetModule,
+    PanelModule,
+    CommonModule,
+  ],
   templateUrl: 'basics-page.component.html',
 })
 export class BasicsPageComponent implements OnInit {
+  public nameLower: string = 'pablo';
+  public nameUpper: string = 'PABLO';
+  public nameComplete: string = 'pAblO nUñEz';
+
   constructor() {}
 
   ngOnInit() {}
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ButtonModule,
+    CardModule,
+    FieldsetModule,
+    PanelModule,
+  ],
   templateUrl: './products.component.html',
 })
 export class ProductsComponent {}
