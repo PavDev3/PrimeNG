2a95f4e41af498b82fffd27792adfa9e2af0ee25
Pavdev3 <pnunfe@gmail.com>
2024-05-02 10:03:00 +0200

    Update Angular dependencies and add @angular/localize

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -1,10 +1,17 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import localeEnGB from '@angular/common/locales/en-GB';
+import localeEsES from '@angular/common/locales/es-ES';
+import localeFrCA from '@angular/common/locales/fr-CA';
 import { Component, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { PanelModule } from 'primeng/panel';
 import { ProductsComponent } from '../../products.component';
+
+registerLocaleData(localeEnGB);
+registerLocaleData(localeFrCA);
+registerLocaleData(localeEsES);
 
 @Component({
   standalone: true,
@@ -23,6 +30,7 @@ export class BasicsPageComponent implements OnInit {
   public nameLower: string = 'pablo';
   public nameUpper: string = 'PABLO';
   public nameComplete: string = 'pAblO nUñEz';
+  public customDate: Date = new Date();
 
   constructor() {}
 
