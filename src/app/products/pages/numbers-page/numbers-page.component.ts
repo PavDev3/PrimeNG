cc03a498567c8f12f73de2401d28c003beded944
Pavdev3 <pnunfe@gmail.com>
2024-05-03 11:10:26 +0200

    Add number-related components and update numbers-page component

--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
   standalone: true,
   selector: 'app-numbers-page',
   templateUrl: 'numbers-page.component.html',
+  imports: [PanelModule, CommonModule, CardModule],
 })
 export class NumbersPageComponent implements OnInit {
+  public totalSells: number = 123456.789;
+  public percent: number = 0.1234;
   constructor() {}
 
   ngOnInit() {}
