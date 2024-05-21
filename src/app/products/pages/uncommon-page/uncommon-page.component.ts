e03a39f382d7af40bca9abd9268b60415a99ed55
Pavdev3 <pnunfe@gmail.com>
2024-05-21 11:29:25 +0200

    chore: Add i18nSelect Pipe to uncommon-page component and update dependencies

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 import { FieldsetModule } from 'primeng/fieldset';
 import { PanelModule } from 'primeng/panel';
 
@@ -7,9 +8,21 @@ import { PanelModule } from 'primeng/panel';
   standalone: true,
   selector: 'app-uncommon-page',
   templateUrl: 'uncommon-page.component.html',
-  imports: [PanelModule, CommonModule, FieldsetModule],
+  imports: [PanelModule, CommonModule, FieldsetModule, ButtonModule],
 })
 export class UncommonPageComponent implements OnInit {
+  //i18n select
+  public name: string = 'Pablo';
+  public gender: 'male' | 'female' = 'male';
+  public welcomeMap = {
+    male: 'bienvenido',
+    female: 'bienvenida',
+  };
+
+  changeClient() {
+    this.name = 'Maria';
+    this.gender = 'female';
+  }
   constructor() {}
 
   ngOnInit() {}
