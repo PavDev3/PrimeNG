c1af811e93483d91409c6820604d81626d57d6ee
Pavdev3 <pnunfe@gmail.com>
2024-06-06 15:18:55 +0200

    refactor some tittle

--- src/app/products/pages/customspipe/customs-page.component.ts
+++ src/app/products/pages/customspipe/customs-page.component.ts
@@ -1,20 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
-import { CardModule } from 'primeng/card';
-import { FieldsetModule } from 'primeng/fieldset';
-import { PanelModule } from 'primeng/panel';
 
 @Component({
   standalone: true,
   selector: 'customs-page',
-  imports: [
-    RouterOutlet,
-    ButtonModule,
-    CardModule,
-    FieldsetModule,
-    PanelModule,
-  ],
+  imports: [RouterOutlet],
   templateUrl: './customs-page.component.html',
 })
 export class CustomsPageComponent {}
--- src/app/products/pages/customspipe/orderpipe/order-page.component.ts
+++ src/app/products/pages/customspipe/orderpipe/order-page.component.ts
@@ -1,8 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
   standalone: true,
   selector: 'order-page',
   templateUrl: 'order-page.component.html',
+  imports: [
+    CommonModule,
+    ButtonModule,
+    CardModule,
+    FieldsetModule,
+    PanelModule,
+  ],
 })
 export class OrderPageComponent {}
