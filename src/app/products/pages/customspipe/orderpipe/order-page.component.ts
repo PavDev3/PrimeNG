dea59c98298838b01e6c364e2be3a938a9fab1c8
Pavdev3 <pnunfe@gmail.com>
2024-06-10 13:46:34 +0200

    feat: Add split button to order-page component
    This commit adds a split button to the order-page component. The split button provides options for saving, updating, deleting, and navigating to the home page. This enhancement improves the user experience by providing convenient actions for managing orders.

--- src/app/products/pages/customspipe/orderpipe/order-page.component.ts
+++ src/app/products/pages/customspipe/orderpipe/order-page.component.ts
@@ -4,6 +4,8 @@ import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { PanelModule } from 'primeng/panel';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { ToolbarModule } from 'primeng/toolbar';
 import { ToggleCasePipe } from '../../../pipes/toggle-case.pipe';
 
 @Component({
@@ -16,8 +18,29 @@ import { ToggleCasePipe } from '../../../pipes/toggle-case.pipe';
     CardModule,
     FieldsetModule,
     PanelModule,
+    ToolbarModule,
+    SplitButtonModule,
     //Pipes
     ToggleCasePipe,
   ],
 })
-export class OrderPageComponent {}
+export class OrderPageComponent {
+  items = [
+    {
+      label: 'Save',
+      icon: 'pi pi-check',
+    },
+    {
+      label: 'Update',
+      icon: 'pi pi-refresh',
+    },
+    {
+      label: 'Delete',
+      icon: 'pi pi-trash',
+    },
+    {
+      label: 'Home',
+      icon: 'pi pi-home',
+    },
+  ];
+}
