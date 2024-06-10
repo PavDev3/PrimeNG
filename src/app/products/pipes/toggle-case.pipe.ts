2696b0eaf5e4afbf0648e4e732cd49790f5bc227
Pavdev3 <pnunfe@gmail.com>
2024-06-10 13:39:38 +0200

    feat: Improve toggle-case pipe to support both upper and lower case transformations
    This commit modifies the toggle-case pipe to support both upper and lower case transformations. The transform method now accepts a boolean parameter, allowing the user to specify whether the value should be transformed to upper case or lower case. This enhancement provides more flexibility in displaying text in different case formats.

--- src/app/products/pipes/toggle-case.pipe.ts
+++ src/app/products/pipes/toggle-case.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true,
 })
 export class ToggleCasePipe implements PipeTransform {
-  transform(value: string): string {
-    return value.toUpperCase();
+  transform(value: string, toUpper: boolean = false): string {
+    return toUpper ? value.toUpperCase() : value.toLowerCase();
   }
 }
